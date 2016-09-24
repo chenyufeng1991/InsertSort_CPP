c882d8996e36f8425db6c602c4d6a1de107b01d8
陈宇峰 <yufengcode@gmail.com>
2016-09-24 23:48:47 +0800

    Initial Commit

---
+++ InsertSort_C++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  InsertSort_C++
+//
+//  Created by chenyufeng on 9/24/16.
+//  Copyright © 2016 chenyufeng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
